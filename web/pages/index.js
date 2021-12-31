b4bd8b99109fbbc4b841caa7aee815b5617c320b
dannyroberts95 <danielrdhr@gmail.com>
2021-12-31 13:50:58 +0000

    recent posts by category

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,6 +1,4 @@
 import React, { useEffect } from 'react';
-import BoxesPage from '../src/components/boxes';
-import HeroScene from '@/components/HeroScene';
 import Layout from '@/Layouts/Layout';
 import { useRouter } from 'next/router';
 import PageBaseContainer from '@/components/PageBaseContainer';
@@ -8,12 +6,11 @@ import groq from 'groq';
 import client from '@/client';
 import HeroImage from '@/components/HeroImage';
 import { Box, useTheme } from '@mui/system';
-import { ButtonBase, Container, Paper, Stack, Typography } from '@mui/material';
+import { ButtonBase, Container, Fade, Grid, Paper, Stack, Typography } from '@mui/material';
 import TagStack from '@/components/TagStack';
 import CreatedDate from '@/components/CreatedDate';
 import Link from '@/components/Link';
 import RecentPostList from '@/components/RecentPostList';
-import { lighten } from '@mui/material';
 const Index = (props) => {
     console.log(props);
     const { slug, mainImage, title, categories, publishedAt } = props;
@@ -27,49 +24,69 @@ const Index = (props) => {
 
     return (
         <div>
-            <ButtonBase LinkComponent={Link} naked href={`/post/${slug.current}`}>
+            <ButtonBase
+                sx={{ color: theme.palette.primary.main }}
+                LinkComponent={Link}
+                naked
+                href={`/post/${slug.current}`}
+            >
                 <Box width={'100%'} position="relative" mb={5}>
                     <HeroImage image={mainImage} interactive={false} />
 
-                    <Box
-                        width="100%"
-                        component={Paper}
-                        square
-                        elevation={4}
-                        sx={{
-                            position: 'absolute',
-                            bottom: 0,
-                            backgroundColor: 'rgb(18, 18, 18,0.66)'
-                        }}
-                    >
-                        <Container sx={{ p: 4 }}>
-                            <Stack
-                                display="flex"
-                                direction="row"
-                                justifyContent="space-between"
-                                alignitems="center"
-                            >
-                                <TagStack tags={categories} />
-                                <CreatedDate date={publishedAt} />
-                            </Stack>
-                            <Typography
-                                variant={'h2'}
-                                gutterBottom
-                                color="textPrimary"
-                                component={Link}
-                                href={`/post/${slug.current}`}
-                                sx={{ textDecoration: 'none' }}
-                            >
-                                {title}
-                            </Typography>
-                        </Container>
-                    </Box>
+                    <Fade in>
+                        <Box
+                            width="100%"
+                            component={Paper}
+                            square
+                            elevation={4}
+                            sx={{
+                                position: 'absolute',
+                                bottom: 0,
+                                backgroundColor: 'rgb(18, 18, 18,0.66)'
+                            }}
+                        >
+                            <Container sx={{ p: 4 }}>
+                                <Stack
+                                    display="flex"
+                                    direction="row"
+                                    justifyContent="space-between"
+                                    alignitems="center"
+                                >
+                                    <TagStack tags={categories} />
+                                    <CreatedDate date={publishedAt} />
+                                </Stack>
+                                <Typography
+                                    variant={'h2'}
+                                    gutterBottom
+                                    color="textPrimary"
+                                    component={Link}
+                                    href={`/post/${slug.current}`}
+                                    sx={{ textDecoration: 'none' }}
+                                >
+                                    {title}
+                                </Typography>
+                            </Container>
+                        </Box>
+                    </Fade>
                 </Box>
             </ButtonBase>
 
             <PageBaseContainer>
-                <Container >
-                    <RecentPostList />
+                <Container>
+                    <Grid container spacing={4}>
+                        <Grid item container>
+                            <RecentPostList category={'Web'} />
+                        </Grid>
+                        <Grid item container>
+                            <RecentPostList category={'Frontend'} />
+                        </Grid>
+                        <Grid item container>
+                            <RecentPostList category={'Video'} />
+                        </Grid>
+                        <Grid item container>
+                            <RecentPostList category={'Scenography'} />
+                        </Grid>
+                    </Grid>
                 </Container>
             </PageBaseContainer>
         </div>
--- web/src/components/RecentPostList.js
+++ web/src/components/RecentPostList.js
@@ -4,27 +4,41 @@ import PostCard from './PostCard';
 import groq from 'groq';
 import Link from '@/components/Link';
 
-
 import client from '@/client';
 
 const RecentPostList = (props) => {
-    const { ...others } = props;
+    const { category = null, amount = 3, ...others } = props;
     const [postData, setPostData] = useState(null);
     const [error, setError] = useState(null);
     const [fetching, setFetching] = useState(true);
 
+    const buildQuery = () => {
+        console.log(`title == "${category}"`);
+        return !category
+            ? groq`
+            *[_type == "post" && publishedAt < now()]{
+                    title,
+                    slug,
+                    "categories": categories[]->title,
+                    "mainImage": mainImage=>image,
+                    publishedAt,
+                }|order(publishedAt desc)[0...${amount}]
+                `
+            : groq`*[_type == 'post'&&
+                 publishedAt < now() &&
+                *[_type == "category" &&
+                title == "${category}"][0]._id in categories[]._ref]{
+                    title,
+                    slug,
+                    "categories": categories[]->title,
+                    "mainImage": mainImage=>image,
+                    publishedAt,
+                }|order(publishedAt desc)[0...${amount}]`;
+    };
+
     const getPostData = async () => {
         await client
-            .fetch(
-                groq`
-*[_type == "post" && publishedAt < now()]{ title,
-slug,
-"categories": categories[]->title,
-"mainImage": mainImage=>image,
-publishedAt,
-}|order(publishedAt desc)[0...3]
-`
-            )
+            .fetch(buildQuery())
             .then((res) => {
                 console.log(res);
                 if (res) {
@@ -38,33 +52,37 @@ publishedAt,
 
     useEffect(async () => {
         getPostData();
-    }, []);
+    }, [category]);
 
-    if (fetching || error) return null;
-    else
-        return (
-            <Grid item container spacing={2} {...others}>
+    const header = () => {
+        return `${category || 'Recent Posts'}`;
+    };
 
-                <Grid item container justifyContent={'space-between'}>
-                    <Typography variant="h6" gutterBottom>
-                        More Posts
-                    </Typography>
-                    <Button component={Link} href="/posts">
-                        All Posts
-                    </Button>
-                </Grid>
-                {postData.map((post, i) => (
-                    <Grid
-                        key={post.slug.current + i + 'recent_post'}
-                        item
-                        flexBasis="100%"
-                        xs={12}
-                        sm={4}>
-                        <PostCard post={post} />
-                    </Grid>
-                ))}
+    if (fetching || error || !postData) return null;
+
+    return (
+        <Grid item container spacing={2} {...others}>
+            <Grid item container justifyContent={'space-between'}>
+                <Typography variant="h6" gutterBottom>
+                    {header()}
+                </Typography>
+                <Button component={Link} href={category ? `/posts?category=${category}` : '/posts'}>
+                    View All
+                </Button>
             </Grid>
-        );
+            {postData.map((post, i) => (
+                <Grid
+                    key={post.slug.current + i + 'recent_post'}
+                    item
+                    flexBasis="100%"
+                    xs={12}
+                    sm={4}
+                >
+                    <PostCard post={post} />
+                </Grid>
+            ))}
+        </Grid>
+    );
 };
 
 export default RecentPostList;
